3de04e9dfba29ffaa96b9706c839a21ce6dec6f5
itsDurdenn <24308060610640@cetis61.edu.mx>
2026-05-07 12:39:57 -0600

    hecho

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import flet as ft
 
-
 def main(page: ft.Page):
     page.title = "App con boton"
     page.bgcolor = "#ce9dff"
@@ -9,37 +8,9 @@ def main(page: ft.Page):
         "Hola mundo.",
         size=30,
         color="#000000",
-        weight=ft.FontWeight.BOLD,
+        weight=ft.FontWeight.BOLD
     )
-
-    def cambiar_texto(e):
-        mensaje.value = "Has hecho clic en el botón."
-        mensaje.color = "#ffffff"
-        page.update()
-
-    boton1 = ft.Button("Haz clic aquí", on_click=cambiar_texto)
-
-    tarjeta = ft.Container(
-        content=ft.Column(
-            controls=[
-                mensaje,
-                boton1
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            spacing=20
-        ),
-        width=400,
-        height=300,
-        padding=20,
-        border_radius=15,
-        bgcolor=ft.Colors.GREY_100,
-        shadow=ft.BoxShadow(
-            blur_radius=10, spread_radius=1, color=ft.Colors.BLACK26
-        )
-    )
-    page.add(tarjeta)
-
-
+    page.add(mensaje)
+    
 if __name__ == "__main__":
-    ft.run(target=main)
+    ft.app(target=main)
---
+++ app_boton.py
@@ -0,0 +1,43 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "App con boton"
+    page.bgcolor = "#ce9dff"
+
+    mensaje = ft.Text(
+        "Hola mundo.",
+        size=30,
+        color="#000000",
+        weight=ft.FontWeight.BOLD
+    )
+
+    def cambiar_texto(e):
+        mensaje.value = "Has hecho clic en el botón"
+        mensaje.color = "#ff0000"
+        page.update()
+
+    boton1 = ft.Button("Haz clic aquí", on_click=cambiar_texto)
+
+    tarjeta = ft.Container(
+        content=ft.Column(
+            controls=[
+                mensaje,
+                boton1
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=20
+        ),
+        width=400,
+        height=300,
+        padding=20,
+        border_radius=15,
+        bgcolor=ft.Colors.GREY_100,
+        shadow=ft.BoxShadow(
+            blur_radius=10, spread_radius=1, color=ft.Colors.BLACK26
+        )
+    )
+    page.add(tarjeta)
+
+if __name__ == "__main__":
+    ft.app(target=main)
