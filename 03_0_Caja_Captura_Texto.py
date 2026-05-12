498789208deda9c2f679fbedbf185eb1d6ab01b3
itsDurdenn <24308060610640@cetis61.edu.mx>
2026-05-12 12:58:14 -0600

    hecho

--- 03_0_Caja_Captura_Texto.py
+++ 03_0_Caja_Captura_Texto.py
@@ -5,12 +5,19 @@ def main(page: ft.Page):
 
     page.window_width = 320  
     page.window_height = 420
+    page.bgcolor = "#ce9dff"
    
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    mensaje2 = ft.Text("Hola Mundo!", size=16)
+    page.add(mensaje2)
 
     mensaje = ft.Text("Hola, escribe algo abajo 👇", size=16)
 
     caja_texto = ft.TextField(label="Escribe tu mensaje")
+    
+    caja_texto2 = ft.TextField(label="Escribe algo")
+    page.add(caja_texto2)
 
     def cambiar_texto(e):
         mensaje.value = f"Tú escribiste: {caja_texto.value}"  
